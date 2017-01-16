f19b93095330dda0c9cbc6b82599a415389aaafc
NVitanovic <x-team@live.com>
2017-01-16 22:11:28 +0100

    added classes for Redis objects that handle proxmox

---
+++ src/APKScanSharedClasses/RedisProxmox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APKScanSharedClasses
+{
+    public enum eTask { rollbackSnapshot, crateSnapshot }
+    public class RedisProxmox
+    {
+        public string vm_id { get; set; }
+        public eTask task { get; set; }
+        public string master_id { get; set; }
+        public string auth { get; set; }
+    }
+    public class RedisProxmoxResult
+    {
+        public string master_id { get; set; }
+        public DateTime lastRun { get; set; }
+        public eTask lastTask { get; set; }
+        public bool status { get; set; }
+    }
+}
